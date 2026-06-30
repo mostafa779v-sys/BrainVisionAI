4f4ae8a5e0ad29855f9df17d13dab59a139193eb
mostafa779v-sys <mostafa779v@gmail.com>
2026-06-30 16:51:50 +0300

    Update model.py

--- training/model.py
+++ training/model.py
@@ -1,1 +1,15 @@
+import torch.nn as nn
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
 
+def create_model():
+
+    model = efficientnet_b0(
+        weights=EfficientNet_B0_Weights.DEFAULT
+    )
+
+    model.classifier[1] = nn.Linear(
+        model.classifier[1].in_features,
+        4
+    )
+
+    return model
