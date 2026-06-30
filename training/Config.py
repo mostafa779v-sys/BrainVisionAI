dc0a05b215b1d5822b07fba099a692c2f88e8d65
mostafa779v-sys <mostafa779v@gmail.com>
2026-06-30 16:50:37 +0300

    Update Config.py

--- training/Config.py
+++ training/Config.py
@@ -1,10 +1,28 @@
+import torch
+
+# Device
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Image Settings
 IMAGE_SIZE = 224
-BATCH_SIZE = 32
 NUM_CLASSES = 4
 
+# Training Settings
+BATCH_SIZE = 32
+LEARNING_RATE = 0.0001
+EPOCHS = 10
+
+# Dataset Classes
 CLASS_NAMES = [
     "glioma",
     "meningioma",
     "notumor",
     "pituitary"
 ]
+
+# Dataset Paths
+TRAIN_PATH = "dataset/Training"
+TEST_PATH = "dataset/Testing"
+
+# Model Save Path
+MODEL_PATH = "Models/BrainVisionAI_v1.pth"
