e3ac041d83c5ccca057d021d70c98a0d6e012999
mostafa779v-sys <mostafa779v@gmail.com>
2026-06-30 16:54:33 +0300

    Update train.py

--- training/train.py
+++ training/train.py
@@ -1,1 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
+from config import *
+from model import create_model
+from dataset import train_loader, test_loader
+
+model = create_model().to(DEVICE)
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer = optim.Adam(
+    model.parameters(),
+    lr=LEARNING_RATE
+)
+
+print("BrainVisionAI Training Ready")
