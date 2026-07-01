9ec5c9ef4270063f6862439bebaca40db512b781
mostafa779v-sys <mostafa779v@gmail.com>
2026-07-01 17:11:33 +0300

    Update __init__.py

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -1,1 +1,17 @@
+"""
+====================================================
+BrainVisionAI Reporting Package
+====================================================
+"""
 
+from .report import create_report
+
+from .builder import ReportBuilder
+
+__all__ = [
+
+    "create_report",
+
+    "ReportBuilder"
+
+]
